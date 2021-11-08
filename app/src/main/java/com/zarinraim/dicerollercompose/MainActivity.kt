7efad9a4a45d791694b3b1bc1f5e46d24cadb4de
zarin-raim <zarinraim@gmail.com>
2021-11-08 10:09:01 +0100

    Roll button
    Added roll button

--- app/src/main/java/com/zarinraim/dicerollercompose/MainActivity.kt
+++ app/src/main/java/com/zarinraim/dicerollercompose/MainActivity.kt
@@ -3,14 +3,19 @@ package com.zarinraim.dicerollercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.zarinraim.dicerollercompose.ui.theme.DiceRollerComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,10 +33,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceRoller() {
-    Column {
-        Text(text = "1")
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = stringResource(id = R.string.roll))
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "1",
+                fontSize = 30.sp
+            )
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.roll))
+            }
         }
     }
 }
